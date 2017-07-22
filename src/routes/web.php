5f7cd77cbfe4433a3cd4d733d5ff54073f014054
shiichi <s.shiichi311041@gmail.com>
2017-07-22 19:21:50 +0900

    add test hash method

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,9 +11,23 @@
 |
 */
 
+use Illuminate\Support\Facades\Hash;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hash/{plain}', function ($plain) {
+    return Hash::make($plain);
+});
+
+Route::get('/hash/check/{plain}', function ($plain) {
+	$hashed = '$2y$10$L9dPa1QQBPJJLmfoOubQXOLCf9Qa/HeIb6E1KT/cDadYkg.ak9Wsm';
+    return Hash::check($plain, $hashed) ? 1 : 0;
+});
+
+
 
 Auth::routes();
 
